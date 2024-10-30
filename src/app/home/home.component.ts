3cf18dce1930d18850443b3e49005694d2dd9d49
mcshanebrendan <mcshanebrendan@yahoo.com>
2024-10-30 18:22:11 +0000

    making some additons to my home component (while on holiday :) )

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,16 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
   message: string = '';
+  data: any; // To store fetched data
 
   constructor(private http: HttpClient) { }
 
@@ -20,4 +21,17 @@ export class HomeComponent implements OnInit {
     });
   }
 
+  fetchData(): void {
+    this.http.get<any>('http://127.0.0.1:5000/get').subscribe(response => {
+      this.data = response;
+    });
+  }
+
+  addData(): void {
+    const payload = { id: Math.floor(Math.random() * 1000) }; // Generate a random id
+    this.http.post<any>('http://127.0.0.1:5000/add', payload).subscribe(response => {
+      console.log("Data added:", response);
+      this.fetchData(); // Refresh data after adding
+    });
+  }
 }
