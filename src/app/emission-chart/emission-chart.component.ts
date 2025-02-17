2de3319bb82527f0b4b1854b789eba11d5ae831b
mcshanebrendan <mcshanebrendan@yahoo.com>
2025-02-17 15:30:30 +0000

    adding emission chart component

---
+++ src/app/emission-chart/emission-chart.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-emission-chart',
+  standalone: true,
+  imports: [],
+  templateUrl: './emission-chart.component.html',
+  styleUrl: './emission-chart.component.css'
+})
+export class EmissionChartComponent {
+
+}
