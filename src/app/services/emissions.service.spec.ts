83e65d5875cd057709350d2e27494b5867b9cb1e
mcshanebrendan <mcshanebrendan@yahoo.com>
2025-03-30 19:46:39 +0100

    adding unit testing for emissions service, work in progress

--- src/app/services/emissions.service.spec.ts
+++ src/app/services/emissions.service.spec.ts
@@ -1,16 +1,64 @@
 import { TestBed } from '@angular/core/testing';
-
-import { EmissionsService } from './emissions.service';
+import { provideHttpClientTesting, HttpTestingController } from '@angular/common/http/testing';
+import { EmissionsService, EmissionData } from './emissions.service';
+import { AuthService } from './auth.service';
 
 describe('EmissionsService', () => {
   let service: EmissionsService;
+  let httpMock: HttpTestingController;
+  let mockAuthService: jasmine.SpyObj<AuthService>;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    // Create a fake AuthService with just the getToken method
+    const authSpy = jasmine.createSpyObj('AuthService', ['getToken']);
+
+    // Set up test environment and provide mocks
+    TestBed.configureTestingModule({
+      providers: [
+        EmissionsService,
+        provideHttpClientTesting(), // replaces the old HttpClientTestingModule
+        { provide: AuthService, useValue: authSpy }
+      ]
+    });
+
     service = TestBed.inject(EmissionsService);
+    httpMock = TestBed.inject(HttpTestingController);
+    mockAuthService = TestBed.inject(AuthService) as jasmine.SpyObj<AuthService>;
+    mockAuthService.getToken.and.returnValue('mocked-token');
+  });
+
+  afterEach(() => {
+    httpMock.verify(); // make sure there are no pending HTTP requests
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should send a POST request to add-user-emission with auth header', () => {
+    const testData: EmissionData = {
+      category: 'Transport',
+      type: 'Diesel',
+      value: 100
+    };
+
+    service.addEmission(testData).subscribe();
+
+    const req = httpMock.expectOne('http://localhost:5000/add-user-emission');
+    expect(req.request.method).toBe('POST');
+    expect(req.request.headers.get('Authorization')).toBe('Bearer mocked-token');
+    expect(req.request.body).toEqual(testData);
+
+    req.flush({ message: 'Success' });
+  });
+
+  it('should send a GET request to get-user-emissions with auth header', () => {
+    service.getUserEmissions().subscribe();
+
+    const req = httpMock.expectOne('http://localhost:5000/get-user-emissions');
+    expect(req.request.method).toBe('GET');
+    expect(req.request.headers.get('Authorization')).toBe('Bearer mocked-token');
+
+    req.flush([{ category: 'Transport', type: 'Diesel', value: 100 }]);
   });
 });
