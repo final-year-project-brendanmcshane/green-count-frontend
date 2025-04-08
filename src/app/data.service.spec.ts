b328bc1ee06be479bf898717f62eeb86c71a643f
mcshanebrendan <mcshanebrendan@yahoo.com>
2025-04-08 12:07:19 +0100

    added passing tests for data service with mocked http requests

--- src/app/data.service.spec.ts
+++ src/app/data.service.spec.ts
@@ -1,16 +1,52 @@
 import { TestBed } from '@angular/core/testing';
-
 import { DataService } from './data.service';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 
 describe('DataService', () => {
   let service: DataService;
+  let httpMock: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [DataService]
+    });
+
     service = TestBed.inject(DataService);
+    httpMock = TestBed.inject(HttpTestingController);
+  });
+
+  afterEach(() => {
+    httpMock.verify();
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should fetch data using GET', () => {
+    const mockData = [{ id: 1, value: 'Test' }];
+
+    service.getData().subscribe(data => {
+      expect(data).toEqual(mockData);
+    });
+
+    const req = httpMock.expectOne('http://127.0.0.1:5000/get');
+    expect(req.request.method).toBe('GET');
+    req.flush(mockData);
+  });
+
+  it('should add data using POST', () => {
+    const payload = { value: 'New Data' };
+    const mockResponse = { success: true };
+
+    service.addData(payload).subscribe(response => {
+      expect(response).toEqual(mockResponse);
+    });
+
+    const req = httpMock.expectOne('http://127.0.0.1:5000/add');
+    expect(req.request.method).toBe('POST');
+    expect(req.request.body).toEqual(payload);
+    req.flush(mockResponse);
   });
 });
