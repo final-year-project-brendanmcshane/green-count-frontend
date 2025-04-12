35a33be3185fa39239f639d3503c95c0440a75b2
mcshanebrendan <mcshanebrendan@yahoo.com>
2025-04-12 10:56:16 +0100

    working on test for home component

--- src/app/home/home.component.spec.ts
+++ src/app/home/home.component.spec.ts
@@ -1,16 +1,37 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { HomeComponent } from './home.component';
+import { provideHttpClient, HttpClient } from '@angular/common/http';
+import { provideRouter } from '@angular/router';
+import { EmissionsService } from '../services/emissions.service';
+import { AuthService } from '../services/auth.service';
+import { of } from 'rxjs';
+
+// You also need to import this since EmissionChartComponent is a standalone dependency of HomeComponent
+import { EmissionChartComponent } from '../emission-chart/emission-chart.component';
 
 describe('HomeComponent', () => {
   let component: HomeComponent;
   let fixture: ComponentFixture<HomeComponent>;
 
+  const mockEmissionsService = {
+    getUserEmissions: () => of([]),
+    addEmission: () => of({ success: true })
+  };
+
+  const mockAuthService = {
+    logout: () => {}
+  };
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [HomeComponent]
-    })
-    .compileComponents();
+      imports: [HomeComponent, EmissionChartComponent],
+      providers: [
+        provideHttpClient(), // Use non-deprecated version for HttpClient
+        provideRouter([]),
+        { provide: EmissionsService, useValue: mockEmissionsService },
+        { provide: AuthService, useValue: mockAuthService }
+      ]
+    }).compileComponents();
 
     fixture = TestBed.createComponent(HomeComponent);
     component = fixture.componentInstance;
@@ -18,6 +39,6 @@ describe('HomeComponent', () => {
   });
 
   it('should create', () => {
-    expect(component).toBeTruthy();
+    expect(component).toBeTruthy(); // confirms the component bootstraps without crashing
   });
 });
