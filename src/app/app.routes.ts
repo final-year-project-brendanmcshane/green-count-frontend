b4aaff353af6399e6bcd2659d92e075047de66e7
mcshanebrendan <mcshanebrendan@yahoo.com>
2025-04-18 18:16:57 +0100

    adding infromation compoennt to routes, added code for component as well

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,6 +5,7 @@ import { SignupComponent } from './signup/signup.component';
 import { EmissionChatComponent } from './emission-chat/emission-chat.component';
 import { DailyEmissionsComponent } from './daily-emissions/daily-emissions.component';
 import { SettingsComponent } from './settings/settings.component';
+import { InformationComponent } from './information/information.component';
 export const routes: Routes = [
     { path: '', redirectTo: 'login', pathMatch: 'full' }, // Default to login
     { path: 'home', component: HomeComponent }, // Add home component route
@@ -12,5 +13,6 @@ export const routes: Routes = [
     { path: 'login', component: LoginComponent },
     { path: 'chat', component: EmissionChatComponent }, // ✅ New Route for AI Chat
     { path: 'daily-emissions', component: DailyEmissionsComponent },
-    {path: 'settings', component: SettingsComponent}
+    {path: 'settings', component: SettingsComponent},
+    { path: 'information', component: InformationComponent }
 ];
