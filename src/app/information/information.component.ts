e44e4805d79fb4c7f4fb262aa08749d5a253c480
mcshanebrendan <mcshanebrendan@yahoo.com>
2025-04-25 16:30:52 +0100

    adding back button to information component

--- src/app/information/information.component.ts
+++ src/app/information/information.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-information',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink],
   templateUrl: './information.component.html',
   styleUrls: ['./information.component.css']
 })
